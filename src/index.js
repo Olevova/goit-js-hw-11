3c5519bfaccf52be18f1153e44720919da80f3d9
Olevova <olevova1983@gmail.com>
2022-11-03 23:48:04 +0200

    button

--- src/index.js
+++ src/index.js
@@ -1,26 +1,46 @@
 import axios from 'axios';
 import cardHbs from "./templates/card.hbs";
 
-console.log(cardHbs);
+// console.log(cardHbs);
 
 const el = some => document.querySelector(some) 
 
 const API_KEY = "31047925-06104412e98efcf2f7c7fbf45";
 const SITE = 'https://pixabay.com/api/' 
 
-function myApiRequest() {
-    return fetch(`${SITE}?key=${API_KEY}&q=cat&photo&horizontal&safesearch=true`).then(r => r.json())
+// функція пошуку та повернення повернення результатів
+async function myApiRequest(v) {
+    // console.log(`${SITE}?key=${API_KEY}&q=${v}&photo&horizontal&safesearch=true`);
+    const resFetch = await fetch(`${SITE}?key=${API_KEY}&q=${v}&photo&horizontal&safesearch=true`);
+    const resFind = await resFetch.json();
+    console.log(resFind);
+    return resFind
 }
-myApiRequest().then(({hits}) => tolik(hits));
 
-function tolik(v) {
-    console.log(v);
+
+
+// функція малювання сторінки
+function renderPage(value) {
+    // console.log(value);
     // const p = JSON.parse(v);
-    console.log(v);
-    const t = cardHbs({v})
+    // console.log(v);
+    const t = cardHbs({value})
     el(".gallery").innerHTML = t;
 }
 
+
+el("#search-form").addEventListener("submit", searchWord);
+
+function searchWord(e) {
+    e.preventDefault();
+    const searchRequest = e.currentTarget.searchQuery.value;
+    console.log(searchRequest);
+    myApiRequest(searchRequest).then(({ hits }) => { console.log(hits); renderPage(hits) });
+
+    // myApiRequest().then(({value}) => renderPage(value));
+}
+
+// myApiRequest().then(({hits}) => renderPage(hits));
 // axios.get('/user?ID=12345')
 //   .then(function (response) {
 //     // handle success
