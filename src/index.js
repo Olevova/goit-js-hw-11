ededd26e5122e13c78d75817fb462f5d8dc2f408
Olevova <olevova1983@gmail.com>
2022-11-04 19:06:45 +0200

    gallery

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import cardHbs from "./templates/card.hbs";
+import SimpleLightbox from "simplelightbox";
 
 // console.log(cardHbs);
 
@@ -8,24 +9,27 @@ const el = some => document.querySelector(some)
 const API_KEY = "31047925-06104412e98efcf2f7c7fbf45";
 const SITE = 'https://pixabay.com/api/' 
 
-// функція пошуку та повернення повернення результатів
+// функція пошуку та повернення результатів
 async function myApiRequest(v) {
-    // console.log(`${SITE}?key=${API_KEY}&q=${v}&photo&horizontal&safesearch=true`);
-    const resFetch = await fetch(`${SITE}?key=${API_KEY}&q=${v}&photo&horizontal&safesearch=true`);
-    const resFind = await resFetch.json();
+    console.time(myApiRequest);
+    const resFind = await axios.get(`${SITE}?key=${API_KEY}&q=${v}&photo&horizontal&safesearch=true`)
     console.log(resFind);
-    return resFind
+    return resFind.data
 }
+
+
+//     const resFetch = await fetch(`${SITE}?key=${API_KEY}&q=${v}&photo&horizontal&safesearch=true`);
+//     const resFind = await resFetch.json();
+//     console.log(resFind);
+//     return resFind
+// }
 
 
 
 // функція малювання сторінки
 function renderPage(value) {
-    // console.log(value);
-    // const p = JSON.parse(v);
-    // console.log(v);
-    const t = cardHbs({value})
-    el(".gallery").innerHTML = t;
+    const allImage = cardHbs({value})
+    el(".gallery").innerHTML = allImage;
 }
 
 
@@ -36,20 +40,19 @@ function searchWord(e) {
     const searchRequest = e.currentTarget.searchQuery.value;
     console.log(searchRequest);
     myApiRequest(searchRequest).then(({ hits }) => { console.log(hits); renderPage(hits) });
-
+    
     // myApiRequest().then(({value}) => renderPage(value));
 }
 
-// myApiRequest().then(({hits}) => renderPage(hits));
-// axios.get('/user?ID=12345')
-//   .then(function (response) {
-//     // handle success
-//     console.log(response);
-//   })
-//   .catch(function (error) {
-//     // handle error
-//     console.log(error);
-//   })
-//   .finally(function () {
-//     // always executed
-//   });
+el(".gallery").addEventListener("click", onClickImgMakeGallery)
+
+function onClickImgMakeGallery(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== "IMG") {
+        return
+    }
+    
+    let gallery = new SimpleLightbox('.gallery a', { captionsData: "alt", captionDelay: 250 });
+    gallery.on('show.simplelightbox')
+}
+
