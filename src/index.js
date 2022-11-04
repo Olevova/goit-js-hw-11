1d7bf66b984b252e06fd4dccd0f5c1f81d4d445a
Olevova <olevova1983@gmail.com>
2022-11-04 21:09:15 +0200

    notify

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import cardHbs from "./templates/card.hbs";
 import SimpleLightbox from "simplelightbox";
+import Notiflix from 'notiflix';
 
 // console.log(cardHbs);
 
@@ -9,11 +10,11 @@ const el = some => document.querySelector(some)
 const API_KEY = "31047925-06104412e98efcf2f7c7fbf45";
 const SITE = 'https://pixabay.com/api/' 
 
+let page = 1;
+
 // функція пошуку та повернення результатів
 async function myApiRequest(v) {
-    console.time(myApiRequest);
-    const resFind = await axios.get(`${SITE}?key=${API_KEY}&q=${v}&photo&horizontal&safesearch=true`)
-    console.log(resFind);
+    const resFind = await axios.get(`${SITE}?key=${API_KEY}&q=${v}&photo&horizontal&safesearch=true&per_page=40&page=${page}`)
     return resFind.data
 }
 
@@ -38,8 +39,15 @@ el("#search-form").addEventListener("submit", searchWord);
 function searchWord(e) {
     e.preventDefault();
     const searchRequest = e.currentTarget.searchQuery.value;
-    console.log(searchRequest);
-    myApiRequest(searchRequest).then(({ hits }) => { console.log(hits); renderPage(hits) });
+    // console.log(searchRequest);
+    console.log(myApiRequest(searchRequest));
+    myApiRequest(searchRequest).then(({ hits }) => {
+         if (hits < 1) {
+             Notiflix.Notify.warning('Sorry, there are no images matching your search query. Please try again.');
+             return
+    }
+        return renderPage(hits)
+    });
     
     // myApiRequest().then(({value}) => renderPage(value));
 }
