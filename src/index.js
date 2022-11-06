70c0b0a420f59d4156180e16c4f1c8225082a3d3
Olevova <olevova1983@gmail.com>
2022-11-06 13:01:23 +0200

    all2

--- src/index.js
+++ src/index.js
@@ -10,31 +10,23 @@ const el = some => document.querySelector(some)
 
 const API_KEY = "31047925-06104412e98efcf2f7c7fbf45";
 const SITE = 'https://pixabay.com/api/' 
+const PAGESEARCH = 40;
 
 let page = 1;
 let searchRequest = '';
+let totalCardImg = 0;
 
 // функція пошуку та повернення результатів
 async function myApiRequest(v) {
-    const resFind = await axios.get(`${SITE}?key=${API_KEY}&q=${v}&photo&horizontal&safesearch=true&per_page=40&page=${page}`)
+    const resFind = await axios.get(`${SITE}?key=${API_KEY}&q=${v}&photo&horizontal&safesearch=true&per_page=${PAGESEARCH}&page=${page}`)
     return resFind.data
 }
-
-
-//     const resFetch = await fetch(`${SITE}?key=${API_KEY}&q=${v}&photo&horizontal&safesearch=true`);
-//     const resFind = await resFetch.json();
-//     console.log(resFind);
-//     return resFind
-// }
-
 
 
 // функція малювання сторінки
 function renderPage(value) {
     const allImage = cardHbs({ value })
-    console.log(allImage);
     el(".gallery").insertAdjacentHTML("beforeend", allImage);
-    el(".load-more").style.visibility = "visible";
 }
 
 
@@ -45,15 +37,16 @@ function searchWord(e) {
     el(".gallery").innerHTML = "";
     page = 1
     searchRequest = e.currentTarget.searchQuery.value;
-    // console.log(searchRequest);
-    console.log(myApiRequest(searchRequest));
-    myApiRequest(searchRequest).then(({ hits,totalHits }) => {
+    // console.log(myApiRequest(searchRequest));
+    myApiRequest(searchRequest).then(({ hits, totalHits }) => {
+        totalCardImg = totalHits;
          if (hits < 1) {
              Notiflix.Notify.warning('Sorry, there are no images matching your search query. Please try again.');
              return
         }
         Notiflix.Notify.success(`totalHits ${totalHits}`);
         renderPage(hits);
+        el(".load-more").style.visibility = "visible";
         let gallery = new SimpleLightbox('.gallery a');
         gallery.on('show.simplelightbox')
 
@@ -67,22 +60,21 @@ el(".load-more").addEventListener("click", addNewDate);
 function addNewDate(e) {
     e.preventDefault();
     page += 1;
-
-    // const searchRequest = e.currentTarget.searchQuery.value;
-    // console.log(searchRequest);
-    console.log(myApiRequest(searchRequest));
+    totalCardImg -= PAGESEARCH;
     myApiRequest(searchRequest).then(({ hits, totalHits }) => {
-        if (hits < 1) {
+        Notiflix.Notify.info(`Hooray! We found ${totalHits} images.`);
+        if (totalCardImg < 1) {
             Notiflix.Notify.warning('Sorry, there are no images matching your search query. Please try again.');
             return
         }
-        const lessImage = totalHits - 40;
-        if (lessImage < 40) {
+        console.log(totalCardImg);
+        if (totalCardImg < PAGESEARCH) {
             renderPage(hits);
-            el(".load-more").style.visibility = "invisible";
+            Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
+            el(".load-more").style.visibility = "hidden";
             let gallery = new SimpleLightbox('.gallery a');
             gallery.on('show.simplelightbox').refresh()
-
+            console.log(gallery);
         }
         // Notiflix.Notify.success(`totalHits ${totalHits}`);
         renderPage(hits);
